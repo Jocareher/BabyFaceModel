f4be44331cee96634cfbb629116e8bf3f2a0037f
Jocareher <jocareher95@gmail.com>
2024-06-05 18:30:43 +0200

    Add dummy faces to include landmarks in generate_interpolations

--- generate_interpolations.m
+++ generate_interpolations.m
@@ -1,4 +1,4 @@
-function generate_interpolations(mean_mesh, synthetic_meshes, steps)
+function generate_interpolations(mean_mesh, synthetic_meshes, steps, averaged_landmarks)
     % Generate interpolations between the mean mesh and synthetic meshes.
     % mean_mesh: Structure with fields 'verts' and 'faces' representing the mean mesh.
     % synthetic_meshes: Cell array of structures representing the synthetic meshes.
@@ -23,9 +23,21 @@ function generate_interpolations(mean_mesh, synthetic_meshes, steps)
             interpolated_mesh.verts = interpolated_verts;
             interpolated_mesh.faces = mean_mesh.faces;
 
+            % Append the landmarks to the interpolated vertices
+            all_verts = [interpolated_mesh.verts, interpolated_mesh.verts(:, averaged_landmarks)];
+
+            % Create landmark faces (dummy faces to visualize as points)
+            num_original_verts = size(interpolated_mesh.verts, 2);
+            landmark_faces = [(num_original_verts+1:num_original_verts+length(averaged_landmarks))' ...
+                              (num_original_verts+1:num_original_verts+length(averaged_landmarks))' ...
+                              (num_original_verts+1:num_original_verts+length(averaged_landmarks))'];
+
+            % Combine original faces with dummy landmark faces
+            all_faces = [interpolated_mesh.faces, landmark_faces'];
+
             % Save the interpolated mesh to a PLY file
             filename = sprintf('interpolated_mesh_%d_step_%d.ply', i, s);
-            interpolated_mesh_surface = surfaceMesh(interpolated_mesh.verts', interpolated_mesh.faces');
+            interpolated_mesh_surface = surfaceMesh(all_verts', all_faces');
             writeSurfaceMesh(interpolated_mesh_surface, filename);
         end
     end
